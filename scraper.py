15bc3b857b10ad1a9484200a92e74ece3b14a59b
TimKapf <kapferer.tim@icloud.com>
2021-07-24 08:27:39 +0200

    None as return

--- scraper.py
+++ scraper.py
@@ -3,17 +3,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 import time
-import os
+
 
 
 
 def get_number(String):
 	'''This function returns the numbers of type float which appear in the given String. Lieferando.de uses the german format to represent point numbers, therefore "," will be transformed to ".".
 	 Only works for the german format.'''
-
+    #TODO Error potential if more than one , 
 	result = ''.join(x for x in String if x.isdigit() or x == ',')
 	if result == "":
-		return -999.0
+		return None
 	result = float(result.replace(',','.'))
 	return result
 
@@ -21,12 +21,13 @@ def restaurants(Adress):
 	'''This funtion will return a list of tuples. Each tuple represents one restaurant which can be found at the input variable (adress) on Lieferando.de.
 	One tuple has the form (restaurant_name, [type_kitchen1, type_kitchen2, ...], time_of_delivery, delivery_costs, min_order_value, rating, number_of_rating).'''
 
-	PATH = "" # Path where the chromedriver is stored in 
+	PATH = "" # Copy the path of the chromedriver in here 
 	driver = webdriver.Chrome(PATH) 
 	driver.get("https://www.lieferando.de")
 	search = driver.find_element_by_id("imysearchstring")
 	search.send_keys(Adress)
 
+    #TODO Check if there is a nicer way to wait, might also fail if the brower/internet is slow
 	time.sleep(2) # seconds
 
 	driver.find_element_by_id("submit_deliveryarea").click()
@@ -40,43 +41,43 @@ def restaurants(Adress):
 		try:
 			restaurant_name = restaurant.find_element_by_class_name("restaurantname").text
 		except NoSuchElementException:
-			restaurant_name = "###"
+			restaurant_name = None
 
 		try:
 			kitchen =  restaurant.find_element_by_class_name("kitchens").text
 			kitchen = kitchen.split(", ")
 		except NoSuchElementException:
-			kitchen = ["###"]
+			kitchen = None
 
 		try: 
 			delivery_time = restaurant.find_element_by_class_name("avgdeliverytime.avgdeliverytimefull.open").text
 			if delivery_time.startswith("Ab"):
-				delivery_time = -999
+				delivery_time = None
 			else:
 				delivery_time = int(get_number(delivery_time))
 
 		except NoSuchElementException:
-			delivery_time = -999
+			delivery_time = None
 
 		try: 
 			delivery_cost = get_number(restaurant.find_element_by_class_name("delivery-cost.js-delivery-cost.notranslate").text)
 		except NoSuchElementException:
-			delivery_cost = -999.0
+			delivery_cost = None
 
 		try: 
 			min_order = get_number(restaurant.find_element_by_class_name("min-order.notranslate").text)
 		except NoSuchElementException:
-			min_order = -999.0
+			min_order = None
 
 		try:
 			rating = get_number(restaurant.find_element_by_class_name("review-stars-range").get_attribute("style")) / 20
 		except NoSuchElementException:
-			rating = -999.0
+			rating = None
 
 		try:
 			num_of_rating = int(get_number(restaurant.find_element_by_class_name("rating-total").text))
 		except NoSuchElementException:
-			num_of_rating = -999
+			num_of_rating = None
 
 		restaurant_tuple = (restaurant_name, kitchen, delivery_time, delivery_cost, min_order, rating, num_of_rating)
 		restaurants.append(restaurant_tuple)
