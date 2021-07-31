2238c479f78ce943450005d022a86686db07cd1f
Antim8 <petersen.tim@outlook.de>
2021-07-31 14:03:33 +0200

    lets_scrape.py init

--- lets_scrape.py
+++ lets_scrape.py
@@ -1,9 +1,16 @@
 # the main file for starting the process 
 
+import scraper
+import data_refiner
+import visualization
+
 #TODO Input of parameters
 
 #TODO Calling Scraper with desired options
-
+data = scraper.restaurants("Osnabrück")
 #TODO Flatten/Refine information gathered by the scraper
-
+#data = data_refiner.tag_correction(data)
 #TODO Output of data
+#visualization.basic_bar(data)
+visualization.basic_pie(data)
+#visualization.discrete_distribution(data,  ["Italienisch", "Indisch"])
