c1860c5b99b2e323be7d3b4bc4e001b692faa036
TimKapf <kapferer.tim@icloud.com>
2021-08-11 16:30:22 +0200

    lets_scrape update

--- lets_scrape.py
+++ lets_scrape.py
@@ -4,6 +4,8 @@ import scraper
 import data_helper
 import visualization
 
+
+#TODO not integrated yet
 kitchens = {"Asiatisch": ["Sushi", "Japanisch", "Poke bowl", "Indisch", "Thailändisch", "Curry",
                         "Vietnamesisch", "Chinesisch", "Koreanisch", "Dumplings", "Indonesisch", "Pakistanisch"],
         "Orientalisch": ["Türkisch", "Döner", "Falafel", "100% Halal", "Persisch", "Türkische Pizza",
@@ -13,49 +15,176 @@ kitchens = {"Asiatisch": ["Sushi", "Japanisch", "Poke bowl", "Indisch", "Thailä
         "Vegetarisch": ["Vegan"],
         "Cafe & Kuchen": ["Eiscreme", "Snacks", "Kuchen", "Nachspeisen", "Backwaren", "Café", "Frühstück"]}
 
-#TODO Input of parameters
-print("Hello there pls gib in welchen Modus")
-print("1. einzelne Stadt")
-print("2. zwei Städte")
-print("3. mehr als zwei Städte")
-op_1 = int(input())
 
-if op_1 == 1:
+plots_one = ['Pie plot (Amount of kitchens)', 'Bar plot (Amount of kitchens)', 'Bar plot (Averages)']
+plots_two = ['Bar plot (Amount of kitchens)', 'Bar plot (Averages)']
+plots_multiple = ['3D bar plots (Amount of kitchens)', 'Heatmap (press 3 to see more)']
 
-    print("Bitte geben Sie die Stadt an.")
-    city_1 = input()
+#TODO eine pdf oder mehrere
+selected_plots_one = []
+selected_plots_two = []
+selected_plots_three = []
 
-elif op_1 == 2:
+def draw_options(mode: int, number_cities: int, start_range: int=1):
 
-    print("Bitte geben Sie die erste Stadt ein")
-    city_1 = input()
-    print("Bitte geben Sie die zweite Stadt ein")
-    city_2 = input()
+    if mode == 1:
+        print("Separate plots: \n")
+        for plot, i in zip(plots_one, range(1, len(plots_one) + 1)):
+            print(str(i) + ": " + plot + "\n")
+    elif mode == 2:
+        if number_cities == 2:
+            print("Two city plots: \n")
+            for plot, i in zip(plots_two, range(start_range, len(plots_two) + start_range)):
+                print(str(i) + ": " + plot + "\n")
+        elif number_cities > 2:
+            print("Multiple city plots: \n")
+            for plot, i in zip(plots_multiple, range(start_range, len(plots_multiple) + start_range)):
+                print(str(i) + ": " + plot + "\n")
+    elif mode == 3: 
+        draw_options(1, number_cities)
+        draw_options(2, number_cities, len(plots_one) + 1)
 
-elif op_1 == 3:
+def avg_options() -> list:
+    print("Enter \n0: All\n1: Average delivery time per kitchen\n2: Average delivery cost per kitchen\n3: Average minimum order cost per kitchen\n4: Average rating per kitchen\n")
+    selections = []
+    while True:
+        avg = input()
+        if str.lower(avg) == 'x':
+            break
+        elif str.lower(avg) == '0':
+            selections = [1, 2, 3, 4]
+            break
+        selections.append(int(avg))
 
-    citys = []
+    return selections
+        
+def get_plots(mode: int, number_cities: int, selection: int, scraped_cities: list, name_cities: list, start_selection: int=1):
+    if mode == 1:
+        if selection == 1:
+            for s_city, n_city in zip(scraped_cities, name_cities):
+                selected_plots_one.append(visualization.basic_pie(s_city, n_city))
+        if selection == 2:
+            for s_city, n_city in zip(scraped_cities, name_cities):
+                selected_plots_one.append(visualization.basic_bar(s_city, n_city))
+        if selection == 3:
+            selection_avg = avg_options()
+            for s_city, n_city in zip(scraped_cities, name_cities):
+                for avg_type in selection_avg:
+                    avg_type += 1
+                    selected_plots_one.append(visualization.avg_bar(s_city, avg_type, n_city)) 
+    elif mode == 2:
+        if number_cities == 2:
+            if selection == start_selection: 
+                selected_plots_two.append(visualization.kitchen_difference(scraped_cities[0], scraped_cities[1], name_cities[0], name_cities[1]))
+            elif selection == start_selection + 1:
+                #TODO 'Bar plot (Averages: press 3 to see more or press 4 to select same ones)' 
+                selection_avg = avg_options()
+                for avg_type in selection_avg:
+                    avg_type += 1
+                    selected_plots_two.append(visualization.avg_difference(scraped_cities[0], scraped_cities[1], name_cities[0], name_cities[1], avg_type))
+        if number_cities > 2:
+            if selection == start_selection:
+                #TODO maybe specify kitchen 
+                selected_plots_three.append(visualization.kitchen_distribution_3D(scraped_cities, name_cities))
+            elif selection == start_selection + 1:
+                selected_plots_three.append(visualization.heatmap(scraped_cities, name_cities))
+    elif mode == 3:
+        if selection < 4:
+            get_plots(1, number_cities, selection, scraped_cities, name_cities) 
+        else:
+            get_plots(2, number_cities, selection, scraped_cities, name_cities, 4) 
 
-    print("Nennen Sie die Städte, die Sie vergleichen möchten getrentt mit Enter oder geben Sie 'x' zum Beenden ein")
+print("Hello. You can get plots for one, two or multiple cities.")
+print("Please enter at least one adress. Type 'x' to stop. \n")
+citys = []
 
-    while('x' != input()):
-        citys.append(input())
+while True:
+    city = input()
+    if str.lower(city) == "x":
+        break
+    citys.append(city)
     
-    print("Möchten Sie für jede Stadt einzeln die Graphen haben oder nur die Vergleichsgraphen? (true oder false)")
-    op_overflow = bool(input())
+# Scrape city information 
+citys_scraped = []
+for city in citys:
+    citys_scraped.append(scraper.restaurants(city))
 
+print("Enter 1, 2 or 3.")
+if len(citys_scraped) > 2:
+    print("Please select number. \n 1: Each city separately. \n 2: Plots with all cities represented. \n 3:  Separately and all cities represented.")
+    decision = int(input())
+ 
+elif len(citys_scraped) == 2:
+    print("Please select number. \n 1: Each city separately. \n 2: Plots with both cities represented.  \n 3: Separately and both cities represented.")
+    decision = int(input())
 else:
-    print("Das war keine gültige Operation, bitte starten Sie das Programm erneut")
+    decision = 1
+
+draw_options(decision, len(citys_scraped))
+
+print("\nEnter the numbers of plots you are interested in or select 0 for all plots. Stop with 'x'.")
+
+selection = []
+while True:
+    plot_type = input()
+    if str.lower(plot_type) == "x":
+        break
+    elif str.lower(plot_type) == "0":
+        selection = 0
+        break
+    selection.append(int(plot_type))
+
+if selection == 0:
+    for s in range(1, 7):
+        get_plots(decision, len(citys_scraped), s, citys_scraped, citys)
+else:
+    for s in selection:
+        get_plots(decision, len(citys_scraped), s, citys_scraped, citys)
+    
+if selected_plots_one:
+    visualization.get_pdf(selected_plots_one, 'plotsone.pdf')
+if selected_plots_two:
+    visualization.get_pdf(selected_plots_two, "plotstwo.pdf")
+if selected_plots_three:
+    visualization.get_pdf(selected_plots_three, "plotsmultiple.pdf")
 
 
-if op_1 < 3:
-    data_1 = scraper.restaurants(city_1)
-    data_2 = scraper.restaurants(city_2) if op_1 == 2 else None
 
-if op_1 < 3 or op_overflow:
-    print("Bestätigen Sie die Graphen, die Sie haben möchten mit 1 oder 0")
 
-    """..."""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+    
+    
+
+
+
+    
+
+
+
+
 
 
 
